6b6c2e9a9772026b29df0378fc640312256ddd7e
kinaura258756-byte <kinaura258756@student.smkn1kandeman.sch.id>
2025-10-21 09:33:15 +0700

    Add files via upload

---
+++ Praktik3.1_kinaura_xpplg2/Praktik3.1_kinaura_xpplg2/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Jumlah
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Input angka pertama dan kedua
+            Console.Write("Masukkan angka pertama: ");
+            int angka1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Masukkan angka kedua: ");
+            int angka2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\n--- Hasil Operasi Penjumlahan ---");
+
+            // PENJUMLAHAN (+)
+            Console.WriteLine($"{angka1} + {angka2} = {angka1 + angka2}");
+            
+
+        }
+    }
+}
