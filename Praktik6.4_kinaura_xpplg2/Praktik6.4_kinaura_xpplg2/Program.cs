ce9a7a99cdf7c4353f9564306ea1b7fc65308dda
kinaura258756-byte <kinaura258756@student.smkn1kandeman.sch.id>
2025-10-21 11:07:23 +0700

    Add files via upload

---
+++ Praktik6.4_kinaura_xpplg2/Praktik6.4_kinaura_xpplg2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Praktik6._4_kinaura_xpplg2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
