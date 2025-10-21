179b0fc8b3e5e6a03c8dd45ff504979488e30fce
kinaura258756-byte <kinaura258756@student.smkn1kandeman.sch.id>
2025-10-21 14:35:02 +0700

    Add files via upload

---
+++ Praktik_7.2_fungsi/Praktik_7.2_fungsi/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Praktik_7._2_fungsi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
